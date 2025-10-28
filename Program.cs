a3247e45d144afefedbf352e7a50db5b3247cdc3
fmarcelomorais <marcelomorais.program@gmail.com>
2025-10-27 23:52:28 -0300

    feat: ajustando criação de jogares no program

--- Program.cs
+++ Program.cs
@@ -4,45 +4,29 @@ using MontandoTimes.Utils;
 
 var jogadores = new List<IJogador>
 {
-        new Jogador { Nome = "Alice", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 3.5 },
-        new Jogador { Nome = "Albuquerque", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 2.5 },
-
-        new Jogador { Nome = "Bruno", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 2.8 },
-        new Jogador { Nome = "Otávio", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 2.4 },
-
-        new Jogador { Nome = "Eduardo", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 2.5 },
-        new Jogador { Nome = "Erick", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 2.25 },
-
-        new Jogador { Nome = "Carlos", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 4.1 },
-        new Jogador { Nome = "Marcos", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 2.9 },
-
-        new Jogador { Nome = "Ricardo", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 3.3 },
-        new Jogador { Nome = "Daniel", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 3.9 },
-
-
-        new Jogador { Nome = "Natan", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 3.2 },
-        new Jogador { Nome = "Felipe", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 4.3 },
-
-        new Jogador { Nome = "Gustavo", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 3.1 },
-        
-        new Jogador { Nome = "Henrique", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 2.7 },
-    
-        new Jogador { Nome = "Igor", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 5.0 },
-        new Jogador { Nome = "Paulo", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 4.0 },
-
-        new Jogador { Nome = "João", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 3.4 },
-        new Jogador { Nome = "Samuel", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 2.6 },
-        
-        new Jogador { Nome = "Kleber", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 3.6 },
-        new Jogador { Nome = "Victor", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 4.2 },
-    
-        new Jogador { Nome = "Lucas", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 4.8 },
-        new Jogador { Nome = "Thiago", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 3.7 },
-
-        new Jogador { Nome = "Balbo", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 1.5 },
-        new Jogador { Nome = "Canela", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 1.7 },
-        new Jogador { Nome = "Canela de Taça", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 2.7 },
-        
+    new Jogador { Nome = " maniesta", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 3.5 },
+    new Jogador { Nome = " Daniel", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 4.0 },
+    new Jogador { Nome = " Joseph", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 2.5 },
+    new Jogador { Nome = " Matheus Lopes", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 5.0 },
+    new Jogador { Nome = " Kauan(c.pedro)", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 3.0 },
+    new Jogador { Nome = " Flad", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 3.5 },
+    new Jogador { Nome = " Wladi", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 4.5 },
+    new Jogador { Nome = " Jeffg2", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 4.0 },
+    new Jogador { Nome = " desio", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 3.0 },
+    new Jogador { Nome = " Wesdras", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 4.5 },
+    new Jogador { Nome = " Márcio", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 4.0 },
+    new Jogador { Nome = " Fabio cr7", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 4.5 },
+    new Jogador { Nome = " Jefferson Veneza", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 4.5 },
+    new Jogador { Nome = " Victor G2", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 3.5 },
+    new Jogador { Nome = " Rudger", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 3.0 },
+    new Jogador { Nome = " Wanderson", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 3.0 },
+    new Jogador { Nome = " Mateus rache", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 5.0 },
+    new Jogador { Nome = " PedroH", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 2.7 },
+    new Jogador { Nome = " Matheus (c.pedro)", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 4.2 },
+    new Jogador { Nome = " Neném", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 3.8 },
+    new Jogador { Nome = " Keldery", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 4.1 },
+    new Jogador { Nome = " Joermeson", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 3.9 },
+    new Jogador { Nome = " Mailton", Posicao = EPosicaoJogador.NaoDefinido, Experiencia = 4.3 },
 
 };
 var posicoesJogadore = new List<EPosicaoJogador>
